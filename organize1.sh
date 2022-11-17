9cc59c4f21d0f5bba4d41829ecefe81f66b5696d
Nathan Edwards <edwardsnj@gmail.com>
2022-11-17 10:26:33 -0500

    tweak version handling

--- organize1.sh
+++ organize1.sh
@@ -25,6 +25,7 @@ mkdir -p "$SUMM"
 touch "$MZML.cksum"
 touch "$PSM.cksum"
 touch "$MZID.cksum"
+touch "$VERSIONS"
 
 for f in "$@"; do
   case "$f" in
@@ -32,10 +33,10 @@ for f in "$@"; do
     *.mzid.gz) echo "$f"; mv -f "$f" "$MZID";;
     *.psm)     echo "$f"; mv -f "$f" "$PSM";;
     *.qcmetrics.tsv) echo "$f"; [ -s "$QCMETRICS" ] && tail -n 1 "$f" >> "$QCMETRICS" || cat "$f" > "$QCMETRICS" ;;
-    *.versions.log) echo "$f"; [ -s "$VERSIONS" ] && cat "$f" >> "${VERSIONS}.tmp" || cat "$f" > "${VERSIONS}.tmp" ;;
+    *.versions.log) echo "$f"; cat "$f" >> "${VERSIONS}" ;;
     *.cksum) echo "$f"; grep '.mzML.gz$' "$f" >> "$MZML.cksum"; grep '.mzid.gz$' "$f" >> "$MZID.cksum"; grep '.psm$' "$f" >> "$PSM.cksum";;
   esac
 done
 
-sort -u "${VERSIONS}.tmp" > "$VERSIONS"
-rm -f "${VERSIONS}.tmp"
+sort -u "${VERSIONS}" > "${VERSIONS}.tmp"
+mv -f "${VERSIONS}.tmp" "${VERSIONS}"
